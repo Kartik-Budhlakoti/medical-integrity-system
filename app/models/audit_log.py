8281c404e9343b35e0da0ebca85dc6bd7fb57dae
Kartik Budhlakoti <professorkarma17@gmail.com>
2026-05-31 19:41:39 +0530

    feat: add AuditLog model

---
+++ app/models/audit_log.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column , Integer , String, DateTime , ForeignKey , Enum
+from datetime import datetime , timezone
+from app.database import Base
+from sqlalchemy.orm import relationship
+
+class AuditLog(Base):
+    __tablename__ = "audit_logs"
+    id = Column(Integer , primary_key=True , index=True)
+    user_id = Column(Integer , ForeignKey("users.id") , nullable=True)
+    action = Column(String , nullable=False)
+    entity_type = Column(String , nullable=False)
+    entity_id = Column(Integer , nullable=False)
+    result = Column(Enum ("SUCCESS", "FAILURE"), nullable=False)
+    ip_address = Column(String , nullable=True)
+    created_at = Column(DateTime , default= lambda: datetime.now(timezone.utc))
+    user = relationship("User" , foreign_keys=[user_id])
