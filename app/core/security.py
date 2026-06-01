f601f3fcee5bf87ede30885fe03ae0e526655e47
Kartik Budhlakoti <professorkarma17@gmail.com>
2026-06-01 09:30:35 +0530

    feat: add password hashing utilities

---
+++ app/core/security.py
@@ -0,0 +1,8 @@
+from passlib.context import CryptContext
+pwd_context = CryptContext(schemes=["bycrypt"] , deprecated="auto")
+
+def hash_password(password: str)->str:
+    return pwd_context.hash(password)
+
+def pwd_verify(plain_password:str, hashed_password:str) -> bool:
+    return pwd_context.verify(plain_password , hashed_password)
