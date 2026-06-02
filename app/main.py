ebcfdc78933e904419f3ad9ffdaef111970b49a8
Kartik Budhlakoti <professorkarma17@gmail.com>
2026-06-02 11:13:29 +0530

    feat: add user creation endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-
+from app.routers.user import router as user_router
 app = FastAPI(
     title="Medical Integrity System",
     version="1.0.0"
 )
 
+app.include_router(user_router)
+
 @app.get("/health")
 def health_check():
     return {"status" : "ok"}
+
---
+++ app/routers/user.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends , HTTPException
+from app.schemas import user
+from app.database import get_db
+from sqlalchemy.orm import Session 
+from app.core.security import hash_password
+from app.models.user import User
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.post("/create" , response_model=user.UserResponse)
+def create_user(user_data : user.UserCreate , db: Session = Depends(get_db)):
+    existing = db.query(User).filter(User.email == user_data.email).first()
+    if existing:
+        raise HTTPException(status_code=400 , detail="Email already registered")
+    new_user = User(
+        full_name = user_data.full_name,
+        email = user_data.email ,
+        pass_hash = hash_password(user_data.password),
+        role_id = user_data.role_id
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
