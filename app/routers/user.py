122177430ca4b52ff1383f3090613e75ed53a4e7
Kartik Budhlakoti <professorkarma17@gmail.com>
2026-06-09 20:42:23 +0530

    feat: add audit logging to creation endpoint

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends , HTTPException
+from fastapi import APIRouter, Depends , HTTPException , Request
 from app.schemas import user
 from app.schemas.token import TokenData 
 from app.database import get_db
@@ -6,16 +6,27 @@ from sqlalchemy.orm import Session
 from app.core.security import hash_password
 from app.models.user import User
 from app.core.dependencies import get_current_user
+from app.core.audit import log_action
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/create" , response_model=user.UserResponse)
-def create_user(user_data : user.UserCreate , db: Session = Depends(get_db), current: TokenData = Depends(get_current_user)):
+def create_user(request : Request,user_data : user.UserCreate , db: Session = Depends(get_db), current: TokenData = Depends(get_current_user)):
     if current.role not in ["SuperAdmin" , "Admin"]:
         raise HTTPException(status_code=403 , detail="Not authorized")
     existing = db.query(User).filter(User.email == user_data.email).first()
     if existing:
+        log_action(
+            db=db,
+            user_id= current.user_id,
+            action= "USER_CREATION_FAILED",
+            entity_type = "users",
+            entity_id =current.user_id, 
+            result= "FAILURE",
+            ip_address=request.client.host
+        )
         raise HTTPException(status_code=400 , detail="Email already registered")
+
     new_user = User(
         full_name = user_data.full_name,
         email = user_data.email ,
@@ -25,4 +36,14 @@ def create_user(user_data : user.UserCreate , db: Session = Depends(get_db), cur
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+    
+    log_action(
+            db=db,
+            user_id= current.user_id,
+            action= "USER_CREATION_SUCCESSFUL",
+            entity_type = "users",
+            entity_id =current.user_id, 
+            result= "SUCCESS",
+            ip_address=request.client.host
+        )
     return new_user
